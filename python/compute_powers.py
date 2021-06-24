790fc96062e1cb55385e97cb38ccc2c512be9492
mike <mshrieve@gmail.com>
2021-06-23 18:13:35 -0700

    fixed powers

--- python/compute_powers.py
+++ python/compute_powers.py
@@ -1,10 +1,14 @@
-import math
-a = ['{0:0{1}}'.format(round(math.pow(2, 2**-(i+1))*10**18), 16)
-     for i in range(64)]
+from decimal import Decimal, getcontext
 
-# 16 hex is 8 bytes = 64 bits :)
-a = ['{0:0{1}}'.format(round(math.pow(2, 2**-(i+1))*10**18), 16)
-     for i in range(64)]
+def power(x): 
+    return getcontext().power(2,x)
 
+e = Decimal((0, (0, 1), 18))
+f = Decimal((0, (0, 1), -18))
+
+a = ['{0:0{1}x}'.format(int(e * power(f * power(i))), 16)
+     for i in range(62)]
 b = ''.join(a)
-bytes = "\x" + '\\x'.join([b[i:i+2] for i in range(0, len(b), 2)])
+
+bytes = "\\x" + '\\x'.join([b[i:i+2] for i in range(0, len(b), 2)])
+print(bytes)
